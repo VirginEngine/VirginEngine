b67100644fb8fe7705c75d87e1bd2edd2aa7fd93
Jakub <jakubhanula0@gmail.com>
2025-01-07 15:46:19 +0100

    0.3.2.0

---
+++ src/editor/hierarchy/components/setComponents.jsx
@@ -0,0 +1,16 @@
+import { Transform } from "./Transform"
+
+export const setComponents = (props) => {
+  window.editor.setInspector(<div>
+    <div>
+      <h2
+        style={{
+          marginLeft: 12
+        }}
+      >
+        {props.name}
+      </h2>
+    </div>
+    <Transform {...props} />
+  </div>)
+}
